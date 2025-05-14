fa17015f3a8f2f7998e0dc43a857a1dafa35bb74
Bramasetio <bramasetioal@gmail.com>
2025-05-14 07:48:40 +0700

    Alpha 1.0.23
    Add SFX Player On Collicion

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,86 @@
+
+
+const scriptsInEvents = {
+
+	async Mainmenu_Event6_Act1(runtime, localVars)
+	{
+		window.stop()
+	},
+
+	async Main_Event9_Act1(runtime, localVars)
+	{
+
+	},
+
+	async Main_Event10_Act2(runtime, localVars)
+	{
+
+	},
+
+	async Main_Event11_Act2(runtime, localVars)
+	{
+
+	},
+
+	async Main_Event12_Act2(runtime, localVars)
+	{
+
+	},
+
+	async Main_Event13_Act2(runtime, localVars)
+	{
+
+	},
+
+	async Main_Event14_Act2(runtime, localVars)
+	{
+
+	},
+
+	async Main_Event15_Act2(runtime, localVars)
+	{
+
+	},
+
+	async Main_Event16_Act2(runtime, localVars)
+	{
+
+	},
+
+	async Main_Event17_Act2(runtime, localVars)
+	{
+
+	},
+
+	async Main_Event18_Act2(runtime, localVars)
+	{
+
+	},
+
+	async Main_Event19_Act2(runtime, localVars)
+	{
+
+	},
+
+	async Main_Event20_Act2(runtime, localVars)
+	{
+
+	},
+
+	async Main_Event50_Act1(runtime, localVars)
+	{
+		console.log("I Hope You there by my side Nas.");
+	},
+
+	async Main_Event52_Act1(runtime, localVars)
+	{
+alert ("You`re Hungry");
+	},
+
+	async Main_Event54_Act5(runtime, localVars)
+	{
+		console.log("I Love You Nas.");
+	}
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
